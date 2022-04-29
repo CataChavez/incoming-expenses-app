b98c9de2ad8f99a829f34f77e2adeb0d6b83b453
cataChavez <catherin.chavez@zentagroup.com>
2022-04-29 00:02:13 -0400

    Register Form implemented

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  myForm: FormGroup = this.fb.group({
+    name: ['nombre', Validators.required],
+    email: ['mail', Validators.required],
+    password: ['123456', Validators.required],
+  })
+
 
   ngOnInit(): void {
+
+  }
+
+  createUser() {
+    console.log(this.myForm);
+    console.log(this.myForm.valid);
+    console.log(this.myForm.value);
+    if (this.myForm.valid) {
+      console.log('formulario valido');
+      this.myForm.markAllAsTouched();
+      this.myForm.reset();
+    }
+
+  }
+
+  isValid(field: string){ 
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
   }
 
 }
